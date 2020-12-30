35af1ece6de1341b623d2cc1a92429fbdb966982
RENZO <martinez.renzo@pucp.edu.pe>
2020-12-30 18:13:49 -0500

    cambio de ruta de back

--- src/dataAccess/API.js
+++ src/dataAccess/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "http://54.204.35:80",
   crossDomain: true,
   headers: {
     "Content-Type": "application/json",
